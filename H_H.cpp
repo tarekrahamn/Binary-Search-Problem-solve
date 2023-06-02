4c0e1de72cef7599004d9b612491959e7bdea1e1
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-06-02 23:40:18 +0600

    Add files via upload

---
+++ H_H.cpp
@@ -0,0 +1,91 @@
+/*
+PROBLEM LINK :
+https://codeforces.com/problemset/problem/978/C
+
+*/
+/*
+ 
+               Starting with the name of almighty ALLAH 
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**
+            In the name of Allah, Most Gracious, Most Merciful             
+                   লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ 
+                        Author:  tarek200 
+                        Created: 02.06.2023 
+                Practice is the only shortcut to improve  
+**/
+#include <bits/stdc++.h>
+#define   Tarek           ios::sync_with_stdio(false);cin.tie(0);
+#define   ll              long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int>
+#define   vll             vector<ll>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   test            ll t;cin >> t;while(t--)
+#define   pb            push_back
+#define   pp             pop_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   exit            return ;
+#define   next            continue;
+#define   B               break;
+#define  checkmate        return 0;
+#define   nl              "\n"
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a/gcd(a,b))*b
+#define   mod             1000000007
+#define   PI              3.14159265358979323846
+#define   all(x)         (x).begin(), (x).end()
+#define   des(x)         (x).rbegin(), (x).rend()
+#define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   FOR(i,a,b)      for(ll i=a;i<b;i++)
+#define   LOOP(i,a,b)     for(ll j=a;j<b;j++)
+#define   LOOP2(i,a,b)    for(ll i=a;i<=b;i++)
+#define   ROF(i, a, b)    for (ll i = a; i >b; i--)
+const long long int N     = (ll) 1e7+1;
+const long long int M     = (ll) 1e9+5;
+using namespace std;
+const ull MOD = 1000003;
+void run(){
+    ll n ,Q;
+    cin >> n >> Q;
+    vll a(n);
+    //vll b(n);
+    LOOP2(i,1,n){
+        cin >> a[i];
+        a[i]+=a[i-1];
+    }
+    while(Q--){
+        ll x;
+        cin >> x;
+        ll l = 0;
+        ll r = n;
+        ll ans = 0;
+        while(l <= r) {
+            ll mid = (l+r)>>1;// l + (r - l) / 2;
+            if(a[mid] >= x) {
+                ans = mid;
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        cout << ans << " ";
+        if (ans){
+            cout << x - a[ans - 1];
+        }
+        cout << nl;
+    }
+}
+int main(){
+    Tarek
+    //test{
+        run();
+    //}
+    checkmate
+}
